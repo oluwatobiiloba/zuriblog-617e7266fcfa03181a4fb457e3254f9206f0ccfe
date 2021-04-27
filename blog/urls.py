bf87aa53f671c1c5bae7ae0eaac6d8a9d06b8c8d
oluwatobiiloba <ooluwatobiiloba@gmail.com>
2021-04-27 12:55:30 +0100

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    BlogListView,
+    BlogUpdateView,
+    BlogDetailView,
+    BlogDeleteView,
+    BlogCreateView,
+)
+
+
+urlpatterns = [
+path('<int:pk>/edit/',BlogUpdateView.as_view(), name='post_edit'), 
+path('<int:pk>/',BlogDetailView.as_view(), name='post_detail'), 
+path('<int:pk>/delete/',BlogDeleteView.as_view(), name='post_delete'),
+path('new/', BlogCreateView.as_view(),name = 'article_new'),
+path('', BlogListView.as_view(), name='post_list'),
+
+]
