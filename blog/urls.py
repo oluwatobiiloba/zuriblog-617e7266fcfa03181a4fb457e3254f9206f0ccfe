307ebae1d0de4e3fd21b74e19cba0f62d9849deb
oluwatobiiloba <ooluwatobiiloba@gmail.com>
2021-05-20 20:27:10 +0100

    undo

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 path('<int:pk>/edit/',BlogUpdateView.as_view(), name='post_edit'), 
 path('<int:pk>/',BlogDetailView.as_view(), name='post_detail'), 
 path('<int:pk>/delete/',BlogDeleteView.as_view(), name='post_delete'),
-path('<int:pk>/new/', BlogCreateView.as_view(),name = 'post_new'),
+path('new/', BlogCreateView.as_view(),name = 'post_new'),
 path('', BlogListView.as_view(), name='post_list'),
 
 ]
